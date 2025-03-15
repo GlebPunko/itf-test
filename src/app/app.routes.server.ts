bdbfbba149fd7716948ba80a160ec8dc29f928ac
Gleb Punko <hleb.punko01@gmail.com>
2025-03-15 14:55:11 +0300

    init app with csr, ssr and ssg.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'contacts',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'company',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Server,
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'contacts',
+        component: ContactsComponent
+    },
+    {
+        path: 'company',
+        component: CompanyComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+   })
+   export class AppRoutingModule { }
